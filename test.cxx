5fe3174f18f379bafdc73d695d8103c497d15623
James Harvey <jamesdillonharvey@users.noreply.github.com>
2023-10-10 10:02:41 +0100

    Update test.cxx

--- test.cxx
+++ test.cxx
@@ -6,7 +6,7 @@
 int main(int argc, char **argv) {
   void *context = zmq_init(1);
   void *socket = zmq_socket(context, ZMQ_SUB);
-  zmq_connect(socket, "tcp://127.0.0.1:12345");
+  zmq_connect(socket, "tcp://127.0.0.1:40500");
   zmq_setsockopt(socket, ZMQ_SUBSCRIBE, "", 0);
 
   char string[1000] = "";
